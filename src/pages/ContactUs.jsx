04b2ab6bc232aa063f8c0a759705765f079e327a
m-shehzad <shehzadboii@gmail.com>
2022-12-10 01:25:59 +0530

    contact us page overhauled

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,35 +1,64 @@
-import React from 'react'
-import '../components/contactUs/contactUs.css'
+import React, {useState} from 'react'
+import '../components/contactUs/contactUs.css';
+import TextBox from '../components/contactUs/TextBox';
 
 export default function ContactUs() {
-  return (
-    <>
-    <div>ContactUs</div>
-    <h1>Hello world</h1>
-    <h2 class="title contactTitle stg3">Let's drive the future, together</h2>
-        <div class="box-container">
-            <h3>Submit your Application</h3>
-            <form autocomplete="off" action="/recruitment" method="POST" class="stg3">
-            <input type="text" placeholder="Your Name" name="Name" required autocomplete="off"></input>
-            <input type="tel" placeholder="Your Number" name="Number" required autocomplete="off"></input>
-            <input type="text" placeholder="Email" name="Email" required autocomplete="off"></input>
-            <select name="department" required>
-                <option value="" selected disabled>Interested department</option>
-                <option value="Mechanical">Mechanical</option>
-                <option value="Electronics">Electrical</option>
-                <option value="Computer science">Electronics & Computer Science</option>
-                <option value="Electronics">Creatives & Management</option>
-            </select>
-            <input class='focus'type="text" placeholder="CV/Resume/Portfolio URL (optional)" name="CV" autocomplete="off"></input>
-                <input class='focus' type="text" placeholder="LinkedIn URL (optional)" name="Linkedin" autocomplete="off"></input>
-                <textarea name="Message"
-                    placeholder="Why do you want to join us? (You can also mention your career interests and skills here)"
-                    id="" cols="30" rows="10" required autocomplete="off"></textarea>
-                <div class="btn-container">
-                    <input type="submit" value="Submit" class="btn"></input>
-                </div>
-            </form>
+    const [credentials, setCredentials] = useState({
+        name: '',
+        number: '',
+        email: '',
+        cv: '',
+        linkedin: '',
+        why: ''
+    });
+
+    return (
+        <>
+        <h2 className='border-white border-solid border-b block text-6xl p-7 text-center text-white'>
+            Let's drive the future, together
+        </h2>
+
+        <div id='contact-us' className='contact-us-container'>
+            <p className='p-0 text-white text-2xl p-4'>Submit your application </p>
+            <TextBox 
+            type='text'
+            placeholder='Your Name'
+            value = {credentials.name}
+            valueSetter = {setCredentials}
+            />
+            <TextBox 
+            type='number'
+            placeholder='Your Number'
+            value = {credentials.number}
+            valueSetter = {setCredentials}
+            />
+            <TextBox 
+            type='email'
+            placeholder='Your email'
+            value = {credentials.email}
+            valueSetter = {setCredentials}
+            />
+            <TextBox 
+            type='text'
+            placeholder='CV/Resume/Portfolio URL (optional)'
+            value = {credentials.resume}
+            valueSetter = {setCredentials}
+            />
+            <TextBox 
+            type='text'
+            placeholder='LinkedIn Profile URL (optional)'
+            value = {credentials.linkedin}
+            valueSetter = {setCredentials}
+            />
+            <TextBox 
+            type='text'
+            placeholder='Why do you want to join us?'
+            value = {credentials.why}
+            valueSetter = {setCredentials}
+            />
+
+            
         </div>
-    </>
-  )
-}
+        </>
+    )
+    }
