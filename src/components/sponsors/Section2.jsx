6757bfda6d4cd5ab1ec10f24ad0736a78baf2e07
Godnon Dsilva <godnondsilva@gmail.com>
2022-12-10 18:25:50 +0530

    Add brochure link

--- src/components/sponsors/Section2.jsx
+++ src/components/sponsors/Section2.jsx
@@ -4,22 +4,12 @@ const Section2 = () => {
 			<div className='text-3xl text-greenColor pb-4'>Our Brochure</div>
 			<div className='text-xl'>
 				<a
-					href='VIEW'
+					href='https://drive.google.com/file/d/1xPP_Yfj90VJWwGikbjEa6OB46NbesAfv/view?usp=sharing'
 					className='hover:text-greenColor hover:text-greenColorHover'
 					target='_blank'
 					rel='noreferrer'
 				>
 					View Online
-				</a>
-			</div>
-			<div className='text-xl'>
-				<a
-					href='DOWNLOAD'
-					className='hover:text-greenColor hover:text-greenColorHover'
-					target='_blank'
-					rel='noreferrer'
-				>
-					Download
 				</a>
 			</div>
 		</div>
